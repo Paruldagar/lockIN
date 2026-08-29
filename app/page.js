1e1c1cfdb2286deb582f0d3295bc3daef11e9499
Parul <parull0912@gmail.com>
2026-08-29 14:25:35 +0530

    Create Home component for Output Tracker page

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1>Output Tracker</h1>
+      <p>Coming soon...</p>
+    </div>
+  );
+}
